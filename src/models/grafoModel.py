9addbc91290d66d556f3cb71133ddfe245051cf1
CristianDavid <cristianescobarcdec@gmail.com>
2022-05-11 18:58:06 -0500

    crud basico completado

--- src/models/grafoModel.py
+++ src/models/grafoModel.py
@@ -2,7 +2,7 @@ from typing import Optional
 from pydantic import BaseModel
 
 class Grafo(BaseModel):
-    id: Optional[str]
+    id: Optional[str] 
     name: str
     descripcion: str
     nodes: list [dict]
--- src/routes/grafos.py
+++ src/routes/grafos.py
@@ -10,30 +10,35 @@ grafo = APIRouter()
 
 # Consulta todos los grafos
 @grafo.get("/grafos")
-def find_all_grafos() :
+def find_all_grafos():
     return grafosEntity(db.grafos.find())
 
+
 # Consultad un grafo por id
-@grafo.get("/grafos/{id}") 
-def find_grafo( id : str ) :
+@grafo.get("/grafos/{id}")
+def find_grafo(id: str):
     print(id)
     return grafoEntity(db.grafos.find_one({'id': id}))
 
+
 # Crea un nuevo grafo
 @grafo.post("/grafos")
-def create_grafo(grafo : Grafo) :
+def create_grafo(grafo: Grafo):
     new_grafo = dict(grafo)
     id = db.grafos.insert_one(new_grafo).inserted_id
     nuevo = db.grafos.find_one({'_id': id})
     return grafoEntity(nuevo)
 
+
 # Actualiza los datos de un grafo por su id
 @grafo.put("/grafos/{id}")
-def update_grafo():
-    return "Hola"
+def update_grafo(id: str, grafo: Grafo):
+    db.grafos.find_one_and_update({'id': id}, {'$set': dict(grafo)})
+    return grafoEntity(db.grafos.find_one({'id': id}))
+
 
 # Elimina un grafo por su id
 @grafo.delete("/grafos/{id}")
-def delete_grafo( id : str ) :
+def delete_grafo(id: str):
     grafoEntity(db.grafos.find_one_and_delete({'id': id}))
-    return Response(status_code = HTTP_204_NO_CONTENT)
+    return Response(status_code=HTTP_204_NO_CONTENT)
